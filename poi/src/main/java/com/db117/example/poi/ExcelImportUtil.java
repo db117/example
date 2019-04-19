f9162196f8ce75017517fea31945e79cd0d1cc35
在那遥远的瞬间 <z351622948@163.com>
2019-04-19 14:15:24 +0800

    添加poi导入,已经数据校验

--- poi/src/main/java/com/db117/example/poi/ExcelImportUtil.java
+++ poi/src/main/java/com/db117/example/poi/ExcelImportUtil.java
@@ -1,9 +1,9 @@
 package com.db117.example.poi;
 
 import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.poi.excel.ExcelFileUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ooxml.util.SAXHelper;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.openxml4j.opc.PackageAccess;
@@ -25,7 +25,6 @@ import org.xml.sax.XMLReader;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
@@ -53,15 +52,54 @@ public class ExcelImportUtil {
      */
     private Consumer<Map<String, String>> consumer;
     /**
+     * 是否读取第一行
+     */
+    private boolean readFirst;
+    /**
+     * 是否读取空行
+     */
+    private boolean readNullRaw;
+    /**
      * 是否只读第一个sheet页
      */
     private boolean readFirstSheetOnly;
 
+    /**
+     * @param file     读取的文件
+     * @param headers  头数组
+     * @param consumer 消费者
+     */
+    public ExcelImportUtil(File file
+            , String[] headers
+            , Consumer<Map<String, String>> consumer) {
+        this(file, headers, consumer
+                , false, false, false);
+    }
 
-    public ExcelImportUtil(OPCPackage xlsxPackage, String[] headers, Consumer<Map<String, String>> consumer) {
-        this.xlsxPackage = xlsxPackage;
+    /**
+     * @param file               读取的文件
+     * @param headers            头数组
+     * @param consumer           消费者
+     * @param readFirst          是否读取第一行
+     * @param readNullRaw        readNullRaw
+     * @param readFirstSheetOnly 是否只读第一个sheet页
+     */
+    public ExcelImportUtil(File file
+            , String[] headers
+            , Consumer<Map<String, String>> consumer
+            , boolean readFirst
+            , boolean readNullRaw
+            , boolean readFirstSheetOnly) {
+        try {
+            this.xlsxPackage = OPCPackage.open(file, PackageAccess.READ);
+        } catch (InvalidFormatException e) {
+            log.error(e.getMessage(), e);
+        }
         this.headers = headers;
         this.consumer = consumer;
+        this.readFirst = readFirst;
+        this.readNullRaw = readNullRaw;
+        this.readFirstSheetOnly = readFirstSheetOnly;
     }
 
     /**
@@ -75,19 +113,13 @@ public class ExcelImportUtil {
     public static <T> List<T> doImport(Class<T> tClass
             , File file
             , String[] header) {
-        check(file);
-
         List<T> list = new ArrayList<>();
-        try (OPCPackage p = OPCPackage.open(file, PackageAccess.READ)) {
-            ExcelImportUtil excelImportUtil = new ExcelImportUtil(
-                    p
-                    , header
-                    // 转换为对象,并添加到集合中
-                    , map -> list.add(BeanUtil.mapToBean(map, tClass, true)));
-            excelImportUtil.process();
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        }
+        ExcelImportUtil excelImportUtil = new ExcelImportUtil(
+                file
+                , header
+                // 转换为对象,并添加到集合中
+                , map -> list.add(BeanUtil.mapToBean(map, tClass, true)));
+        excelImportUtil.process();
         return list;
     }
 
@@ -100,47 +132,63 @@ public class ExcelImportUtil {
      */
     public static List<Map<String, String>> doImport(File file
             , String[] header) {
-        check(file);
-
         List<Map<String, String>> list = new ArrayList<>();
-        try (OPCPackage p = OPCPackage.open(file, PackageAccess.READ)) {
-            ExcelImportUtil excelImportUtil = new ExcelImportUtil(
-                    p
-                    , header
-                    , list::add);
-            // 执行解析
-            excelImportUtil.process();
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-        }
+        ExcelImportUtil excelImportUtil = new ExcelImportUtil(
+                file
+                , header
+                , list::add);
+        // 执行解析
+        excelImportUtil.process();
         return list;
     }
 
+    public static void doImport(File file
+            , String[] headers
+            , Consumer<Map<String, String>> consumer) {
+        ExcelImportUtil excelImportUtil = new ExcelImportUtil(
+                file
+                , headers
+                , consumer);
+        // 执行解析
+        excelImportUtil.process();
+    }
+
     /**
-     * 解析Excel
-     *
-     * @throws IOException  如果从包中读取数据失败
-     * @throws SAXException 如果解析XML数据失败
+     * 解析Excel,并关闭流
      */
-    public void process() throws IOException, OpenXML4JException, SAXException {
-        // 只读字符表
-        ReadOnlySharedStringsTable strings = new ReadOnlySharedStringsTable(this.xlsxPackage);
-        // Xssf读取
-        XSSFReader xssfReader = new XSSFReader(this.xlsxPackage);
-        // 样式表
-        StylesTable styles = xssfReader.getStylesTable();
-        // 读取Excel
-        XSSFReader.SheetIterator iter = (XSSFReader.SheetIterator) xssfReader.getSheetsData();
-        while (iter.hasNext()) {
-            try (InputStream stream = iter.next()) {
-                processSheet(styles
-                        , strings
-                        , new SimpleSheetContentsHandler(headers, consumer)
-                        , stream);
+    public void process() {
+        try {
+            // 只读字符表
+            ReadOnlySharedStringsTable strings = new ReadOnlySharedStringsTable(this.xlsxPackage);
+            // Xssf读取
+            XSSFReader xssfReader = new XSSFReader(this.xlsxPackage);
+            // 样式表
+            StylesTable styles = xssfReader.getStylesTable();
+            // 读取Excel
+            XSSFReader.SheetIterator iter = (XSSFReader.SheetIterator) xssfReader.getSheetsData();
+            while (iter.hasNext()) {
+                try (InputStream stream = iter.next()) {
+                    processSheet(styles
+                            , strings
+                            , new SimpleSheetContentsHandler(headers
+                                    , consumer
+                                    , readFirst
+                                    , readNullRaw)
+                            , stream);
+                }
+                // 当自读第一个sheet是结束
+                if (readFirstSheetOnly) {
+                    break;
+                }
             }
-            // 当自读第一个sheet是结束
-            if (readFirstSheetOnly) {
-                break;
+        } catch (IOException | SAXException | OpenXML4JException e) {
+            log.error(e.getMessage(), e);
+        } finally {
+            // 关流
+            try {
+                this.xlsxPackage.close();
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
             }
         }
     }
@@ -183,6 +231,9 @@ public class ExcelImportUtil {
         }
     }
 
+    /**
+     * 数据处理
+     */
     private class SimpleSheetContentsHandler implements SheetContentsHandler {
         /**
          * 当前行号
@@ -221,8 +272,7 @@ public class ExcelImportUtil {
          */
         SimpleSheetContentsHandler(String[] headers
                 , Consumer<Map<String, String>> consumer) {
-            this.headers = headers;
-            this.consumer = consumer;
+            this(headers, consumer, false, false);
         }
 
         /**
@@ -291,19 +341,6 @@ public class ExcelImportUtil {
             }
             // 设置值
             rowData.put(headers[currentCol], formattedValue);
-        }
-    }
-
-    public static void check(File file) {
-        // 判断是否是07版Excel
-        try (InputStream is = new FileInputStream(file)) {
-            boolean xlsx = ExcelFileUtil.isXlsx(is);
-            if (!xlsx) {
-                // 文件不是07版Excel
-                throw new RuntimeException("file is not OOXML(07Excel,xlsx)");
-            }
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
         }
     }
 }
---
+++ util/src/main/java/com/db117/example/util/DataCheck.java
@@ -0,0 +1,159 @@
+package com.db117.example.util;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.StrUtil;
+import lombok.Data;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.groups.Default;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 数据校验
+ *
+ * @author 大兵
+ * @date 2019/1/2318:27
+ */
+public class DataCheck {
+    private static Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+    private DataCheck() {
+    }
+
+    /**
+     * 校验数据(抛出异常)
+     *
+     * @param t 数据
+     */
+    public static <T> void valid(T t) {
+        List<String> list = validMessage(t);
+        if (CollectionUtil.isNotEmpty(list)) {
+            throw new RuntimeException(StrUtil.join(",", list));
+        }
+    }
+
+    /**
+     * 校验数据(抛出异常)
+     *
+     * @param t      数据
+     * @param groups 校验分组
+     */
+    public static <T> void valid(T t, Class<?>... groups) {
+        List<String> list = validMessage(t, groups);
+        if (CollectionUtil.isNotEmpty(list)) {
+            throw new RuntimeException(StrUtil.join(",", list));
+        }
+    }
+
+    /**
+     * 校验数据
+     * 返回所有异常信息
+     *
+     * @param t 数据
+     * @return 所有异常信息
+     */
+    public static <T> List<String> validMessage(T t) {
+        List<String> list = new ArrayList<>();
+        validator.validate(t, Default.class).forEach(v -> {
+            list.add(v.getMessage());
+        });
+        return list;
+    }
+
+    /**
+     * 校验数据
+     * 返回所有异常信息
+     *
+     * @param t      数据
+     * @param groups 校验分组
+     * @return 所有异常信息
+     */
+    public static <T> List<String> validMessage(T t, Class<?>... groups) {
+        List<String> list = new ArrayList<>();
+        validator.validate(t, groups).forEach(v -> {
+            list.add(v.getMessage());
+        });
+        return list;
+    }
+
+    /**
+     * 是否校验通过
+     *
+     * @param t 数据
+     * @return 是否通过
+     */
+    public static <T> boolean validBool(T t) {
+        Set<ConstraintViolation<T>> violationSet = validator.validate(t, Default.class);
+        return violationSet.size() == 0;
+    }
+
+    /**
+     * 是否校验通过
+     *
+     * @param t      数据
+     * @param groups 校验分组
+     * @return 是否通过
+     */
+    public static <T> boolean validBool(T t, Class<?>... groups) {
+        Set<ConstraintViolation<T>> violationSet = validator.validate(t, groups);
+        return violationSet.size() == 0;
+    }
+
+    /**
+     * 校验数据是否全部b不为空
+     *
+     * @param data 需要校验的对象
+     * @return 是否都不为空
+     */
+    public static boolean checkNotNull(Object data) {
+        AtomicLong total = new AtomicLong();
+        Map<String, Object> map = BeanUtil.beanToMap(data);
+        map.forEach((s, o) -> {
+            if (o == null) {
+                total.getAndIncrement();
+            }
+        });
+        return total.get() == 0;
+    }
+
+    /**
+     * 校验数据是否全部b不为空
+     *
+     * @param data 需要校验的对象
+     * @param num  不为空的属性个数
+     * @return 判断不为空的属性是否大于等于total
+     */
+    public static boolean check(Object data, int num) {
+        AtomicLong total = new AtomicLong();
+        Map<String, Object> map = BeanUtil.beanToMap(data);
+        map.forEach((s, o) -> {
+            if (o != null) {
+                total.getAndIncrement();
+            }
+        });
+
+        return total.get() >= num;
+    }
+
+    public static void main(String[] args) {
+        @Data
+        class Testt {
+            String s1;
+            Integer integer;
+        }
+        Testt testt = new Testt();
+        testt.setS1("1");
+        testt.setInteger(1);
+        System.out.println(checkNotNull(testt));
+        testt.setS1(null);
+        System.out.println(checkNotNull(testt));
+
+    }
+}
