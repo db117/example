52110f887697fe38ced673a3d0fb101501c48083
在那遥远的瞬间 <z351622948@163.com>
2019-06-03 17:27:22 +0800

    LeetCode-Solution12

--- leetcode/src/main/java/com/db117/example/leetcode/Solution12.java
+++ leetcode/src/main/java/com/db117/example/leetcode/Solution12.java
@@ -52,11 +52,28 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Solution12 {
     public static void main(String[] args) {
-        System.out.println(intToRoman(1));
+        System.out.println(intToRoman(3333));
     }
 
     public static String intToRoman(int num) {
+        // 数字映射
+        int[] nums = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] s = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-        return "";
+        StringBuilder res = new StringBuilder("");
+
+        for (int i = 0; i < nums.length; i++) {
+            int temp = num / nums[i];
+            // 除尽继续下次循环
+            if (temp == 0) {
+                continue;
+            }
+            // 拼接罗马数字
+            for (int j = 0; j < temp; j++) {
+                res.append(s[i]);
+            }
+            num = num % nums[i];
+        }
+        return res.toString();
     }
 }
