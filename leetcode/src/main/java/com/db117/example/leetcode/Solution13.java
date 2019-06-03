02fa78f621f4ddcc86323e259d90c61086f90341
在那遥远的瞬间 <z351622948@163.com>
2019-06-03 17:49:41 +0800

    LeetCode-Solution13

---
+++ leetcode/src/main/java/com/db117/example/leetcode/Solution13.java
@@ -0,0 +1,95 @@
+package com.db117.example.leetcode;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+ * <p>
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+ * <p>
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+ * <p>
+ * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+ * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。
+ * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+ * 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "III"
+ * 输出: 3
+ * 示例 2:
+ * <p>
+ * 输入: "IV"
+ * 输出: 4
+ * 示例 3:
+ * <p>
+ * 输入: "IX"
+ * 输出: 9
+ * 示例 4:
+ * <p>
+ * 输入: "LVIII"
+ * 输出: 58
+ * 解释: L = 50, V= 5, III = 3.
+ * 示例 5:
+ * <p>
+ * 输入: "MCMXCIV"
+ * 输出: 1994
+ * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+ *
+ * @author db117
+ * @date 2019/6/3
+ **/
+@Slf4j
+public class Solution13 {
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("MCMXCIV"));
+    }
+
+    public static int romanToInt(String s) {
+        // 数字映射
+        int[] nums = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] roman = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        // 返回值
+        int res = 0;
+        char[] chars = s.toCharArray();
+        // 数组下标
+        int index = 0;
+        // 当前是否找到
+        boolean flag;
+
+        // 每一个罗马字符循环
+        for (int i = 0; i < nums.length; i++) {
+            // 当前罗马字符
+            char[] temp = roman[i].toCharArray();
+
+            flag = true;
+            while (flag) {
+                // 循环比较
+                for (int j = 0; j < temp.length; j++) {
+                    if (index + j == chars.length || chars[index + j] != temp[j]) {
+                        // 如果超出数据长度,或者有不相等的则结束本次循环
+                        flag = false;
+                        break;
+                    }
+                    if (j == temp.length - 1) {
+                        // 找到后添加到返回值,并移动下标
+                        res += nums[i];
+                        index += temp.length;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
