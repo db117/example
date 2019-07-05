f103d43a626d5dfdf1e7cf585d76d36b80bf2cfa
在那遥远的瞬间 <z351622948@163.com>
2019-07-05 20:00:15 +0800

    LeetCode-solution75

---
+++ leetcode/src/main/java/com/db117/example/leetcode/Solution76.java
@@ -0,0 +1,29 @@
+package com.db117.example.leetcode;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字母的最小子串。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入: S = "ADOBECODEBANC", T = "ABC"
+ * 输出: "BANC"
+ * 说明：
+ * <p>
+ * 如果 S 中不存这样的子串，则返回空字符串 ""。
+ * 如果 S 中存在这样的子串，我们保证它是唯一的答案。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/minimum-window-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/7/5
+ **/
+@Slf4j
+public class Solution76 {
+    public String minWindow(String s, String t) {
+        return "";
+    }
+}
