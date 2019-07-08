00d17a46958c33f41fe3e4a03735e44559a91665
在那遥远的瞬间 <z351622948@163.com>
2019-07-08 11:30:11 +0800

    LeetCode-solution76

--- leetcode/src/main/java/com/db117/example/leetcode/Solution76.java
+++ leetcode/src/main/java/com/db117/example/leetcode/Solution76.java
@@ -23,7 +23,60 @@ import lombok.extern.slf4j.Slf4j;
  **/
 @Slf4j
 public class Solution76 {
+    public static void main(String[] args) {
+        System.out.println(new Solution76().minWindow("ADOBECODEBANC", "ABC"));
+    }
+
     public String minWindow(String s, String t) {
-        return "";
+        // 转换为数组
+        int[] ints = new int[128];
+        for (int i = 0; i < t.length(); i++) {
+            ints[t.charAt(i)]++;
+        }
+
+        // 左右指针
+        int left = 0, right = 0;
+        // 返回左右指针
+        int resLeft = 0, resRight = -1;
+        // 最小长度
+        int resLen = Integer.MAX_VALUE;
+        // 需要匹配得个数
+        int count = t.length();
+
+        while (right < s.length()) {
+            // 先移动右指针
+            char cRight = s.charAt(right);
+            // 当前数字减一
+            ints[cRight]--;
+
+            // 如果当前数字大于等于0,说明当前位置有字符
+            if (ints[cRight] >= 0) {
+                count--;
+            }
+
+            // 确定左指针
+            while (count == 0) {
+                // 当前窗口大小
+                int curLen = right - left + 1;
+
+                // 当前窗口小于保存的
+                if (curLen < resLen) {
+                    resLen = curLen;
+                    resLeft = left;
+                    resRight = right;
+                }
+
+                char cLeft = s.charAt(left);
+                // 左边减少,对应的数组的数字加一
+                ints[cLeft]++;
+                // 如果对应的数字大一0,则说明需要匹配的字符被删除(没有在匹配的字符串中的数字永远不会大于0)
+                if (ints[cLeft] > 0) {
+                    count++;
+                }
+                left++;
+            }
+            right++;
+        }
+        return s.substring(resLeft, resRight + 1);
     }
 }
---
+++ leetcode/src/main/java/com/db117/example/leetcode/Solution77.java
@@ -0,0 +1,35 @@
+package com.db117.example.leetcode;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+/**
+ * 给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: n = 4, k = 2
+ * 输出:
+ * [
+ * [2,4],
+ * [3,4],
+ * [2,3],
+ * [1,2],
+ * [1,3],
+ * [1,4],
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/combinations
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/7/8
+ **/
+@Slf4j
+public class Solution77 {
+    public List<List<Integer>> combine(int n, int k) {
+        return null;
+    }
+}
