00fe783a89bf5ca4bec6366773a6c4dc0f8ce577
在那遥远的瞬间 <z351622948@163.com>
2019-07-09 20:36:24 +0800

    LeetCode-solution89

---
+++ leetcode/src/main/java/com/db117/example/leetcode/Solution89.java
@@ -0,0 +1,65 @@
+package com.db117.example.leetcode;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 格雷编码是一个二进制数字系统，在该系统中，两个连续的数值仅有一个位数的差异。
+ * <p>
+ * 给定一个代表编码总位数的非负整数 n，打印其格雷编码序列。格雷编码序列必须以 0 开头。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 2
+ * 输出: [0,1,3,2]
+ * 解释:
+ * 00 - 0
+ * 01 - 1
+ * 11 - 3
+ * 10 - 2
+ * <p>
+ * 对于给定的 n，其格雷编码序列并不唯一。
+ * 例如，[0,2,3,1] 也是一个有效的格雷编码序列。
+ * <p>
+ * 00 - 0
+ * 10 - 2
+ * 11 - 3
+ * 01 - 1
+ * 示例 2:
+ * <p>
+ * 输入: 0
+ * 输出: [0]
+ * 解释: 我们定义格雷编码序列必须以 0 开头。
+ *      给定编码总位数为 n 的格雷编码序列，其长度为 2n。当 n = 0 时，长度为 20 = 1。
+ *      因此，当 n = 0 时，其格雷编码序列为 [0]。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/gray-code
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/7/9
+ **/
+@Slf4j
+public class Solution89 {
+    public static void main(String[] args) {
+        System.out.println((int) (1 << 1));
+    }
+
+    public List<Integer> grayCode(int n) {
+        // 初始化时设置好容量
+        List<Integer> res = new ArrayList<>(1 << n);
+        res.add(0);
+        for (int i = 0; i < n; i++) {
+            int size = res.size();
+            // 每次把生成的数组和2的i次方异或
+            // 倒序
+            for (int j = size - 1; j >= 0; j--) {
+                res.add(res.get(j) ^ (1 << i));
+            }
+        }
+        return res;
+    }
+}
