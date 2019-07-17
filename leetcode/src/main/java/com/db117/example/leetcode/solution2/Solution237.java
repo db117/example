74844b089edd570b3736cc0337ff47c775b6ed2e
在那遥远的瞬间 <z351622948@163.com>
2019-07-17 18:01:42 +0800

    237. 删除链表中的节点

--- leetcode/src/main/java/com/db117/example/leetcode/solution2/Solution237.java
+++ leetcode/src/main/java/com/db117/example/leetcode/solution2/Solution237.java
@@ -1,11 +1,51 @@
 package com.db117.example.leetcode.solution2;
 
+import com.db117.example.leetcode.Util.ListNode;
 import lombok.extern.slf4j.Slf4j;
 
 /**
+ * 237. 删除链表中的节点
+ * <p>
+ * 请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点，你将只被给定要求被删除的节点。
+ * <p>
+ * 现有一个链表 -- head = [4,5,1,9]，它可以表示为:
+ * <p>
+ * <p>
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: head = [4,5,1,9], node = 5
+ * 输出: [4,1,9]
+ * 解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+ * 示例 2:
+ * <p>
+ * 输入: head = [4,5,1,9], node = 1
+ * 输出: [4,5,9]
+ * 解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+ *  
+ * <p>
+ * 说明:
+ * <p>
+ * 链表至少包含两个节点。
+ * 链表中所有节点的值都是唯一的。
+ * 给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+ * 不要从你的函数中返回任何结果。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/delete-node-in-a-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
  * @author db117
  * @date 2019/7/17
  **/
 @Slf4j
 public class Solution237 {
+    public void deleteNode(ListNode node) {
+        // 用下一个节点的值覆盖当前节点的值
+        node.val = node.next.val;
+        // 当前节点指向下下个节点
+        node.next = node.next.next;
+    }
 }
