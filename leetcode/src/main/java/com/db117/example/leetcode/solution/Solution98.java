fe0d3c3c86149350bfd435efc6755dbaa9a6d47a
在那遥远的瞬间 <z351622948@163.com>
2019-07-18 19:31:29 +0800

    98. 验证二叉搜索树

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution/Solution98.java
@@ -0,0 +1,63 @@
+package com.db117.example.leetcode.solution;
+
+import com.db117.example.leetcode.Util.TreeNode;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 98. 验证二叉搜索树
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * <p>
+ * 假设一个二叉搜索树具有如下特征：
+ * <p>
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * 2
+ * / \
+ * 1   3
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:
+ * 5
+ * / \
+ * 1   4
+ *      / \
+ *     3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ *      根节点的值为 5 ，但是其右子节点值为 4 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/7/18
+ **/
+@Slf4j
+public class Solution98 {
+    long temp = Long.MIN_VALUE;
+
+    public boolean isValidBST(TreeNode root) {
+        // 中序遍历有序
+        if (root == null) {
+            return true;
+        }
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+
+        if (root.val > temp) {
+            temp = root.val;
+        } else {
+            // 无序
+            return false;
+        }
+
+        return isValidBST(root.right);
+    }
+}
