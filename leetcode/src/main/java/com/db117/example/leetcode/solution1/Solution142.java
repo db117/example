43628d301bc10f31c658f8cafddfdd2761283ffa
在那遥远的瞬间 <z351622948@163.com>
2019-07-18 14:55:37 +0800

    142. 环形链表 II

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution1/Solution142.java
@@ -0,0 +1,75 @@
+package com.db117.example.leetcode.solution1;
+
+import com.db117.example.leetcode.Util.ListNode;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 142. 环形链表 II
+ * <p>
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ * <p>
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ * <p>
+ * 说明：不允许修改给定的链表。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：tail connects to node index 1
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：head = [1,2], pos = 0
+ * 输出：tail connects to node index 0
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：head = [1], pos = -1
+ * 输出：no cycle
+ * 解释：链表中没有环。
+ * <p>
+ * <p>
+ *  
+ * <p>
+ * 进阶：
+ * 你是否可以不用额外空间解决此题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/linked-list-cycle-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/7/18
+ **/
+@Slf4j
+public class Solution142 {
+    public ListNode detectCycle(ListNode head) {
+        // 先判断是否是循环链表
+        // 如果是循环链表,快指针肯定能追上慢指针
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+
+        // 如果不上循环链表则返回空
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        // 从头,和慢指针一起开始跑,相遇就是目标节点
+        while (head != slow) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
+    }
+}
