99ec27b4b7bfd11007c4cd1c35c20022a991e17f
在那遥远的瞬间 <z351622948@163.com>
2019-07-18 11:32:12 +0800

    141. 环形链表

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution1/Solution141.java
@@ -0,0 +1,72 @@
+package com.db117.example.leetcode.solution1;
+
+import com.db117.example.leetcode.Util.ListNode;
+import com.db117.example.leetcode.Util.ListNodeBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 141. 环形链表
+ * <p>
+ * 给定一个链表，判断链表中是否有环。
+ * <p>
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * <p>
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * <p>
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ * <p>
+ * <p>
+ *  
+ * <p>
+ * 进阶：
+ * <p>
+ * 你能用 O(1)（即，常量）内存解决此问题吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/linked-list-cycle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/7/18
+ **/
+@Slf4j
+public class Solution141 {
+    public static void main(String[] args) {
+        ListNode node = ListNodeBuilder.builder(new int[]{3, 2, 0, -4});
+        System.out.println(new Solution141().hasCycle(node));
+    }
+
+
+    public boolean hasCycle(ListNode head) {
+        // 快慢指针
+        // 如果是循环链表,快指针肯定能追上慢指针
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
