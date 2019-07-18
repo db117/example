73909c1f0c1e577a7b4ca5bd5cd8ee057d2d5090
在那遥远的瞬间 <z351622948@163.com>
2019-07-18 17:17:10 +0800

    添加feign

---
+++ openfeign/src/test/java/com/db117/example/openfeign/FeignTest.java
@@ -0,0 +1,19 @@
+package com.db117.example.openfeign;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = App.class)
+public class FeignTest {
+    @Autowired
+    FeignExample feignExample;
+
+    @Test
+    public void db117() {
+        System.out.println(feignExample.db117());
+    }
+}
