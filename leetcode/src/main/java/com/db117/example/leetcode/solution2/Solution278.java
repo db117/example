071b1de0e6f3a2ba1ccd876dde6795e4c9d2b407
在那遥远的瞬间 <z351622948@163.com>
2019-07-19 18:18:17 +0800

    278. 第一个错误的版本

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution2/Solution278.java
@@ -0,0 +1,53 @@
+package com.db117.example.leetcode.solution2;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 278. 第一个错误的版本
+ * <p>
+ * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+ * <p>
+ * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+ * <p>
+ * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 n = 5，并且 version = 4 是第一个错误的版本。
+ * <p>
+ * 调用 isBadVersion(3) -> false
+ * 调用 isBadVersion(5) -> true
+ * 调用 isBadVersion(4) -> true
+ * <p>
+ * 所以，4 是第一个错误的版本。 
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/first-bad-version
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/7/19
+ **/
+@Slf4j
+public class Solution278 {
+    public int firstBadVersion(int n) {
+        // 二分
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + ((right - left) / 2);
+            if (isBadVersion(mid)) {
+                // 可能溢出
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    boolean isBadVersion(int version) {
+        return true;
+    }
+
+    ;
+}
