502b3059d29f422dde8c6cd585e86c475aba88f8
在那遥远的瞬间 <z351622948@163.com>
2019-07-23 15:20:20 +0800

    412. Fizz Buzz

--- leetcode/src/main/java/com/db117/example/leetcode/solution4/Solution412.java
+++ leetcode/src/main/java/com/db117/example/leetcode/solution4/Solution412.java
@@ -2,12 +2,71 @@ package com.db117.example.leetcode.solution4;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 412. Fizz Buzz
+ * 写一个程序，输出从 1 到 n 数字的字符串表示。
+ * <p>
+ * 1. 如果 n 是3的倍数，输出“Fizz”；
+ * <p>
+ * 2. 如果 n 是5的倍数，输出“Buzz”；
+ * <p>
+ * 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+ * <p>
+ * 示例：
+ * <p>
+ * n = 15,
+ * <p>
+ * 返回:
+ * [
+ * "1",
+ * "2",
+ * "Fizz",
+ * "4",
+ * "Buzz",
+ * "Fizz",
+ * "7",
+ * "8",
+ * "Fizz",
+ * "Buzz",
+ * "11",
+ * "Fizz",
+ * "13",
+ * "14",
+ * "FizzBuzz"
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fizz-buzz
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  * @author db117
  * @date 2019/7/23/023
  **/
 @Slf4j
 public class Solution412 {
+    public static void main(String[] args) {
+        System.out.println(new Solution412().fizzBuzz(15));
+    }
+
+    public List<String> fizzBuzz(int n) {
+        List<String> res = new ArrayList<>(n);
+        for (int i = 1; i <= n; i++) {
+            // 是否是3或5的倍数
+            boolean temp1 = i % 3 == 0;
+            boolean temp2 = i % 5 == 0;
+            if (temp1 && temp2) {
+                res.add("FizzBuzz");
+            } else if (temp1) {
+                res.add("Fizz");
+            } else if (temp2) {
+                res.add("Buzz");
+            } else {
+                res.add(String.valueOf(i));
+            }
+        }
+        return res;
+    }
 }
