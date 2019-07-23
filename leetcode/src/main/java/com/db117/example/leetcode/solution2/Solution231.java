5e59553b6d0dec479f16a5916e9aff20c832b908
在那遥远的瞬间 <z351622948@163.com>
2019-07-23 17:01:01 +0800

    231. 2的幂

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution2/Solution231.java
@@ -0,0 +1,53 @@
+package com.db117.example.leetcode.solution2;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 231. 2的幂
+ * 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 1
+ * 输出: true
+ * 解释: 20 = 1
+ * 示例 2:
+ * <p>
+ * 输入: 16
+ * 输出: true
+ * 解释: 24 = 16
+ * 示例 3:
+ * <p>
+ * 输入: 218
+ * 输出: false
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/power-of-two
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/7/23/023
+ **/
+@Slf4j
+public class Solution231 {
+    public boolean isPowerOfTwo(int n) {
+        // 循环平推
+        if (n == 0) {
+            return false;
+        }
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+        return n == 1;
+    }
+
+    public boolean isPowerOfTwo1(int n) {
+        // 位运算
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+
+    public boolean isPowerOfTwo2(int n) {
+        // 转为二进制,100...才位2的幂
+        return n > 0 && Integer.toBinaryString(n).matches("10*");
+    }
+}
