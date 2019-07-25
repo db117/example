ab90d30e49b2fbbd7e29bd4959c76b231cab75a0
在那遥远的瞬间 <z351622948@163.com>
2019-07-25 10:12:38 +0800

    268. 缺失数字

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution2/Solution268.java
@@ -0,0 +1,42 @@
+package com.db117.example.leetcode.solution2;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 268. 缺失数字
+ * 给定一个包含 0, 1, 2, ..., n 中 n 个数的序列，找出 0 .. n 中没有出现在序列中的那个数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [3,0,1]
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: [9,6,4,2,3,5,7,0,1]
+ * 输出: 8
+ * 说明:
+ * 你的算法应具有线性时间复杂度。你能否仅使用额外常数空间来实现?
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/missing-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/7/24/024
+ **/
+@Slf4j
+public class Solution268 {
+    public static void main(String[] args) {
+        System.out.println(new Solution268().missingNumber(new int[]{9, 6, 4, 2, 3, 5, 7, 0, 1}));
+    }
+
+    public int missingNumber(int[] nums) {
+        int res = 0;
+        // 异或下标和数组的值,只出现一次的那个就是缺失的那个
+        for (int i = 0; i < nums.length; i++) {
+            res ^= i + 1;
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
