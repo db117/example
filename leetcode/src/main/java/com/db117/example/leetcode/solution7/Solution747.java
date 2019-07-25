e5d40b95b6c63b33939194c5bfa88fb2196460eb
在那遥远的瞬间 <z351622948@163.com>
2019-07-25 17:23:00 +0800

    747. 至少是其他数字两倍的最大数

--- leetcode/src/main/java/com/db117/example/leetcode/solution7/Solution747.java
+++ leetcode/src/main/java/com/db117/example/leetcode/solution7/Solution747.java
@@ -1,5 +1,8 @@
 package com.db117.example.leetcode.solution7;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  * 747. 至少是其他数字两倍的最大数
  * 在一个给定的数组nums中，总是存在一个最大元素 。
@@ -37,7 +40,59 @@ package com.db117.example.leetcode.solution7;
  **/
 
 public class Solution747 {
+    public static void main(String[] args) {
+        System.out.println(new Solution747().dominantIndex1(new int[]{3, 6, 1, 0}));
+    }
+
+    public int dominantIndex1(int[] nums) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return 0;
+        }
+        // 最大值
+        int max = Integer.MIN_VALUE;
+        // 第二大
+        int sec = Integer.MIN_VALUE;
+        // 最大值位置
+        int index = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                sec = max;
+                max = nums[i];
+                index = i;
+            } else if (nums[i] > sec) {
+                sec = nums[i];
+            }
+        }
+        if (max >= sec * 2) {
+            return index;
+        }
+        return -1;
+    }
     public int dominantIndex(int[] nums) {
-        return 0;
+        if (nums.length == 0) {
+            return -1;
+        }
+        if (nums.length == 1) {
+            return 0;
+        }
+        // k->值 v->位置
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        // 放入treemap中
+        for (int i = 0; i < nums.length; i++) {
+            treeMap.put(nums[i], i);
+        }
+        // 找到最大的那个数字,以及其位置
+        Map.Entry<Integer, Integer> firstEntry = treeMap.lastEntry();
+        // 找到比最大值小的最大值
+        Integer lowerKey = treeMap.lowerKey(firstEntry.getKey());
+        // 是否是其二倍大
+        if (firstEntry.getKey() >= lowerKey * 2) {
+            return firstEntry.getValue();
+        }
+
+        return -1;
     }
 }
