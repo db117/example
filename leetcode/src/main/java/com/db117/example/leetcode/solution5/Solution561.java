930ac474de906af2c86f19788aca8b56d4de98a0
在那遥远的瞬间 <z351622948@163.com>
2019-07-25 19:57:49 +0800

    561. 数组拆分 I

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution5/Solution561.java
@@ -0,0 +1,39 @@
+package com.db117.example.leetcode.solution5;
+
+import java.util.Arrays;
+
+/**
+ * 561. 数组拆分 I
+ * <p>
+ * 给定长度为 2n 的数组, 你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从1 到 n 的 min(ai, bi) 总和最大。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,4,3,2]
+ * <p>
+ * 输出: 4
+ * 解释: n 等于 2, 最大总和为 4 = min(1, 2) + min(3, 4).
+ * 提示:
+ * <p>
+ * n 是正整数,范围在 [1, 10000].
+ * 数组中的元素范围在 [-10000, 10000].
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/array-partition-i
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/7/25/025
+ **/
+public class Solution561 {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        // 排序,偶数位和
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+}
