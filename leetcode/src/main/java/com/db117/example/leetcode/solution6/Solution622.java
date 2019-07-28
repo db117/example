a1a88e51d170b52947ec94912096418876f965f4
在那遥远的瞬间 <z351622948@163.com>
2019-07-28 16:58:56 +0800

    622. 设计循环队列

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution6/Solution622.java
@@ -0,0 +1,141 @@
+package com.db117.example.leetcode.solution6;
+
+/**
+ * 622. 设计循环队列
+ * 设计你的循环队列实现。 循环队列是一种线性数据结构，其操作表现基于 FIFO（先进先出）原则并且队尾被连接在队首之后以形成一个循环。它也被称为“环形缓冲器”。
+ * <p>
+ * 循环队列的一个好处是我们可以利用这个队列之前用过的空间。在一个普通队列里，一旦一个队列满了，我们就不能插入下一个元素，即使在队列前面仍有空间。但是使用循环队列，我们能使用这些空间去存储新的值。
+ * <p>
+ * 你的实现应该支持如下操作：
+ * <p>
+ * MyCircularQueue(k): 构造器，设置队列长度为 k 。
+ * Front: 从队首获取元素。如果队列为空，返回 -1 。
+ * Rear: 获取队尾元素。如果队列为空，返回 -1 。
+ * enQueue(value): 向循环队列插入一个元素。如果成功插入则返回真。
+ * deQueue(): 从循环队列中删除一个元素。如果成功删除则返回真。
+ * isEmpty(): 检查循环队列是否为空。
+ * isFull(): 检查循环队列是否已满。
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * MyCircularQueue circularQueue = new MycircularQueue(3); // 设置长度为 3
+ * <p>
+ * circularQueue.enQueue(1);  // 返回 true
+ * <p>
+ * circularQueue.enQueue(2);  // 返回 true
+ * <p>
+ * circularQueue.enQueue(3);  // 返回 true
+ * <p>
+ * circularQueue.enQueue(4);  // 返回 false，队列已满
+ * <p>
+ * circularQueue.Rear();  // 返回 3
+ * <p>
+ * circularQueue.isFull();  // 返回 true
+ * <p>
+ * circularQueue.deQueue();  // 返回 true
+ * <p>
+ * circularQueue.enQueue(4);  // 返回 true
+ * <p>
+ * circularQueue.Rear();  // 返回 4
+ *  
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 所有的值都在 0 至 1000 的范围内；
+ * 操作数将在 1 至 1000 的范围内；
+ * 请不要使用内置的队列库。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/design-circular-queue
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/7/28/028
+ **/
+public class Solution622 {
+
+    public static class MyCircularQueue {
+        // 数据
+        int[] queue;
+        // 头节点
+        int head = 0;
+        // 尾节点
+        int tail = 0;
+        // 大小
+        int size = 0;
+
+        /**
+         * Initialize your data structure here. Set the size of the queue to be k.
+         */
+        public MyCircularQueue(int k) {
+            queue = new int[k];
+        }
+
+        /**
+         * Insert an element into the circular queue. Return true if the operation is successful.
+         */
+        public boolean enQueue(int value) {
+            if (isFull()) {
+                return false;
+            }
+            // 尾节点
+            tail = (this.tail + 1) % queue.length;
+            queue[tail] = value;
+            size++;
+            // 如果是第一个则设置头节点为尾节点
+            if (size == 1) {
+                head = tail;
+            }
+            return true;
+        }
+
+        /**
+         * Delete an element from the circular queue. Return true if the operation is successful.
+         */
+        public boolean deQueue() {
+            if (isEmpty()) {
+                return false;
+            }
+            head = (head + 1) % queue.length;
+            size--;
+            return true;
+        }
+
+        /**
+         * Get the front item from the queue.
+         */
+        public int Front() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return queue[head];
+        }
+
+        /**
+         * Get the last item from the queue.
+         */
+        public int Rear() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return queue[tail];
+        }
+
+        /**
+         * Checks whether the circular queue is empty or not.
+         */
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        /**
+         * Checks whether the circular queue is full or not.
+         */
+        public boolean isFull() {
+            return size == queue.length;
+        }
+
+    }
+}
