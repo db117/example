a841e66534600ce16d40fb2bd6933bc4f72ee09d
在那遥远的瞬间 <z351622948@163.com>
2019-08-12 11:29:29 +0800

    454. 四数相加 II

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution4/Solution454.java
@@ -0,0 +1,58 @@
+package com.db117.example.leetcode.solution4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 454. 四数相加 II
+ * 给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+ * <p>
+ * 为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。所有整数的范围在 -228 到 228 - 1 之间，
+ * 最终结果不会超过 231 - 1 。
+ * <p>
+ * 例如:
+ * <p>
+ * 输入:
+ * A = [ 1, 2]
+ * B = [-2,-1]
+ * C = [-1, 2]
+ * D = [ 0, 2]
+ * <p>
+ * 输出:
+ * 2
+ * <p>
+ * 解释:
+ * 两个元组如下:
+ * 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+ * 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/4sum-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/8/11/011
+ **/
+public class Solution454 {
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // 分别保存ab,cd的所有和组合
+        for (int item : A) {
+            for (int value : B) {
+                int temp = item + value;
+                map.put(temp, map.getOrDefault(temp, 0) + 1);
+            }
+        }
+        // 遍历cd的所有和,去找ab的所有和
+        for (int item : C) {
+            for (int value : D) {
+                int temp = item + value;
+                count += map.getOrDefault(-temp, 0);
+            }
+        }
+
+        return count;
+    }
+}
