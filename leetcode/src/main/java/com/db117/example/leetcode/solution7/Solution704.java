a6d1140057670133cabf15be265a096048292f65
在那遥远的瞬间 <z351622948@163.com>
2019-08-12 20:42:36 +0800

    704. 二分查找

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution7/Solution704.java
@@ -0,0 +1,51 @@
+package com.db117.example.leetcode.solution7;
+
+/**
+ * 704. 二分查找
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [-1,0,3,5,9,12], target = 9
+ * 输出: 4
+ * 解释: 9 出现在 nums 中并且下标为 4
+ * 示例 2:
+ * <p>
+ * 输入: nums = [-1,0,3,5,9,12], target = 2
+ * 输出: -1
+ * 解释: 2 不存在 nums 中因此返回 -1
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 你可以假设 nums 中的所有元素是不重复的。
+ * n 将在 [1, 10000]之间。
+ * nums 的每个元素都将在 [-9999, 9999]之间。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-search
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author 117
+ * @date 2019/8/12/012
+ */
+public class Solution704 {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            // 防止超出int最大值
+            int mid = left + (right - left) / 2;
+
+            int temp = nums[mid];
+            if (temp == target) {
+                return mid;
+            } else if (temp > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
