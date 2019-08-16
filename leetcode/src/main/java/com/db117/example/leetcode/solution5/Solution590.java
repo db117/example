e2f36994410c54448370dab17ddcc3d9cfc31f08
在那遥远的瞬间 <z351622948@163.com>
2019-08-16 17:07:02 +0800

    590. N叉树的后序遍历

--- leetcode/src/main/java/com/db117/example/leetcode/solution5/Solution590.java
+++ leetcode/src/main/java/com/db117/example/leetcode/solution5/Solution590.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.Stack;
 
 /**
- * 589. N叉树的前序遍历
- * 给定一个 N 叉树，返回其节点值的前序遍历。
+ * 590. N叉树的后序遍历
+ * 给定一个 N 叉树，返回其节点值的后序遍历。
  * <p>
  * 例如，给定一个 3叉树 :
  * <p>
@@ -16,21 +16,21 @@ import java.util.Stack;
  * <p>
  *  
  * <p>
- * 返回其前序遍历: [1,3,5,6,2,4]。
+ * 返回其后序遍历: [5,6,3,2,4,1].
  * <p>
  *  
  * <p>
  * 说明: 递归法很简单，你可以使用迭代法完成此题吗?
  * <p>
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal
+ * 链接：https://leetcode-cn.com/problems/n-ary-tree-postorder-traversal
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  * @author 117
  * @date 2019/8/16/016
  */
-public class Solution589 {
-    public List<Integer> preorder(Node root) {
+public class Solution590 {
+    public List<Integer> postorder(Node root) {
         List<Integer> res = new ArrayList<>();
         if (root == null) {
             return res;
@@ -43,17 +43,17 @@ public class Solution589 {
         while (!stack.isEmpty()) {
             // 出队
             Node pop = stack.pop();
-            res.add(pop.val);
+            res.add(0, pop.val);
             if (pop.children != null) {
-                // 反向加入栈
-                List<Node> children = pop.children;
-                for (int i = children.size() - 1; i >= 0; i--) {
-                    stack.push(children.get(i));
+                // 入栈
+                for (Node child : pop.children) {
+                    stack.push(child);
                 }
             }
         }
         return res;
     }
+
 
     class Node {
         public int val;
@@ -67,5 +67,4 @@ public class Solution589 {
             children = _children;
         }
     }
-
 }
