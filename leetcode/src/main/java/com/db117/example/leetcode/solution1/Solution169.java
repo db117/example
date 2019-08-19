afd9bd59155700db827ea72f20a458523090acd3
在那遥远的瞬间 <z351622948@163.com>
2019-08-19 18:08:56 +0800

    169. 求众数

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution1/Solution169.java
@@ -0,0 +1,32 @@
+package com.db117.example.leetcode.solution1;
+
+import java.util.Arrays;
+
+/**
+ * 169. 求众数
+ * 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ * <p>
+ * 你可以假设数组是非空的，并且给定的数组总是存在众数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [3,2,3]
+ * 输出: 3
+ * 示例 2:
+ * <p>
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/majority-element
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author 117
+ * @date 2019/8/19/019
+ */
+public class Solution169 {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
