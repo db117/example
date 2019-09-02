28e048bd109b0c32d16eabb415b31a071a299f32
在那遥远的瞬间 <z351622948@163.com>
2019-09-02 16:56:44 +0800

    341. 扁平化嵌套列表迭代器

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution3/Solution341.java
@@ -0,0 +1,101 @@
+package com.db117.example.leetcode.solution3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * 341. 扁平化嵌套列表迭代器
+ * 给定一个嵌套的整型列表。设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
+ * <p>
+ * 列表中的项或者为一个整数，或者是另一个列表。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [[1,1],2,[1,1]]
+ * 输出: [1,1,2,1,1]
+ * 解释: 通过重复调用 next 直到 hasNext 返回false，next 返回的元素的顺序应该是: [1,1,2,1,1]。
+ * 示例 2:
+ * <p>
+ * 输入: [1,[4,[6]]]
+ * 输出: [1,4,6]
+ * 解释: 通过重复调用 next 直到 hasNext 返回false，next 返回的元素的顺序应该是: [1,4,6]。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/flatten-nested-list-iterator
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/9/2/002 16:31
+ */
+public class Solution341 {
+    /**
+     * // This is the interface that allows for creating nested lists.
+     * // You should not implement it, or speculate about its implementation
+     * public interface NestedInteger {
+     * <p>
+     * // @return true if this NestedInteger holds a single integer, rather than a nested list.
+     * public boolean isInteger();
+     * <p>
+     * // @return the single integer that this NestedInteger holds, if it holds a single integer
+     * // Return null if this NestedInteger holds a nested list
+     * public Integer getInteger();
+     * <p>
+     * // @return the nested list that this NestedInteger holds, if it holds a nested list
+     * // Return null if this NestedInteger holds a single integer
+     * public List<NestedInteger> getList();
+     * }
+     */
+    public class NestedIterator implements Iterator<Integer> {
+        Iterator<Integer> iterator;
+
+        public NestedIterator(List<NestedInteger> nestedList) {
+            this.iterator = helper(nestedList).iterator();
+        }
+
+        @Override
+        public Integer next() {
+            return iterator.next();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        public List<Integer> helper(List<NestedInteger> nestedList) {
+            List<Integer> list = new ArrayList<>();
+            nestedList.forEach(nested -> {
+                if (nested.isInteger()) {
+                    // 是数字直接加入
+                    list.add(nested.getInteger());
+                } else {
+                    // 递归添加
+                    list.addAll(helper(nested.getList()));
+                }
+            });
+            return list;
+        }
+    }
+
+
+    /**
+     * Your NestedIterator object will be instantiated and called as such:
+     * NestedIterator i = new NestedIterator(nestedList);
+     * while (i.hasNext()) v[f()] = i.next();
+     */
+
+    public interface NestedInteger {
+
+        // @return true if this NestedInteger holds a single integer, rather than a nested list.
+        public boolean isInteger();
+
+        // @return the single integer that this NestedInteger holds, if it holds a single integer
+        // Return null if this NestedInteger holds a nested list
+        public Integer getInteger();
+
+        // @return the nested list that this NestedInteger holds, if it holds a nested list
+        // Return null if this NestedInteger holds a single integer
+        public List<NestedInteger> getList();
+    }
+}
