41194ca1c04720e6ebf05279997ec97f97c22411
在那遥远的瞬间 <z351622948@163.com>
2019-09-08 16:43:12 +0800

    300. 最长上升子序列

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution3/Solution300.java
@@ -0,0 +1,55 @@
+package com.db117.example.leetcode.solution3;
+
+import java.util.Arrays;
+
+/**
+ * 300. 最长上升子序列
+ * 给定一个无序的整数数组，找到其中最长上升子序列的长度。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [10,9,2,5,3,7,101,18]
+ * 输出: 4
+ * 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+ * 说明:
+ * <p>
+ * 可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
+ * 你算法的时间复杂度应该为 O(n2) 。
+ * 进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-increasing-subsequence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/9/8/008 16:33
+ */
+public class Solution300 {
+    public static void main(String[] args) {
+        System.out.println(new Solution300().lengthOfLIS(new int[]{
+                1, 3, 6, 7, 9, 4, 10, 5, 6
+        }));
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        // 最小值都为1
+        Arrays.fill(dp, 1);
+
+        int max = 0;
+        // 遍历前面的所以数字
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    // 当小于当前值时,取最大的数量
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+        return max;
+    }
+}
