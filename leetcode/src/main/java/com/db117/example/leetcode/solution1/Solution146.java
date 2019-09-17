e5a2299d9e8a7e00d472fcb66ff721205fe5d95e
在那遥远的瞬间 <z351622948@163.com>
2019-09-17 18:43:45 +0800

    146. LRU缓存机制

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution1/Solution146.java
@@ -0,0 +1,75 @@
+package com.db117.example.leetcode.solution1;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 146. LRU缓存机制
+ * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。它应该支持以下操作： 获取数据 get 和 写入数据 put 。
+ * <p>
+ * 获取数据 get(key) - 如果密钥 (key) 存在于缓存中，则获取密钥的值（总是正数），否则返回 -1。
+ * 写入数据 put(key, value) - 如果密钥不存在，则写入其数据值。当缓存容量达到上限时，它应该在写入新数据之前删除最近最少使用的数据值，从而为新的数据值留出空间。
+ * <p>
+ * 进阶:
+ * <p>
+ * 你是否可以在 O(1) 时间复杂度内完成这两种操作？
+ * <p>
+ * 示例:
+ * <p>
+ * LRUCache cache = new LRUCache( 2 // 缓存容量  );
+ * <p>
+ * cache.put(1,1);
+ * cache.put(2,2);
+ * cache.get(1);       // 返回  1
+ * cache.put(3,3);    // 该操作会使得密钥 2 作废
+ * cache.get(2);       // 返回 -1 (未找到)
+ * cache.put(4,4);    // 该操作会使得密钥 1 作废
+ * cache.get(1);       // 返回 -1 (未找到)
+ * cache.get(3);       // 返回  3
+ * cache.get(4);       // 返回  4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lru-cache
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/9/17/017 18:18
+ */
+public class Solution146 {
+    class LRUCache {
+        LinkedHashMap<Integer, Integer> map;
+
+        public LRUCache(int capacity) {
+            map = new MyLinkedHashMap(capacity, 0.75f, true);
+        }
+
+        public int get(int key) {
+            return map.getOrDefault(key, -1);
+        }
+
+        public void put(int key, int value) {
+            map.put(key, value);
+        }
+
+        class MyLinkedHashMap extends LinkedHashMap<Integer, Integer> {
+            int capacity;
+
+            public MyLinkedHashMap(int initialCapacity, float loadFactor, boolean accessOrder) {
+                super(initialCapacity, loadFactor, accessOrder);
+                this.capacity = initialCapacity;
+            }
+
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return capacity < size();
+            }
+        }
+    }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+}
