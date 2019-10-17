f30a84fb95278079864988d771a1685ca9bd6a38
在那遥远的瞬间 <z351622948@163.com>
2019-10-17 20:22:44 +0800

    195. 第十行

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution1/Solution195.java
@@ -0,0 +1,38 @@
+package com.db117.example.leetcode.solution1;
+
+/**
+ * 195. 第十行
+ * 给定一个文本文件 file.txt，请只打印这个文件中的第十行。
+ * <p>
+ * 示例:
+ * <p>
+ * 假设 file.txt 有如下内容：
+ * <p>
+ * Line 1
+ * Line 2
+ * Line 3
+ * Line 4
+ * Line 5
+ * Line 6
+ * Line 7
+ * Line 8
+ * Line 9
+ * Line 10
+ * 你的脚本应当显示第十行：
+ * <p>
+ * Line 10
+ * 说明:
+ * 1. 如果文件少于十行，你应当输出什么？
+ * 2. 至少有三种不同的解法，请尝试尽可能多的方法来解题。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/tenth-line
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/10/17/017 14:22
+ */
+public class Solution195 {
+    // tail -n+10 从第10行开始输出
+    //  tail -n+10 file.txt | head -1
+}
