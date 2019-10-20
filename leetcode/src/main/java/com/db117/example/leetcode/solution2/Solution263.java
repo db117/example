26384eaa4528f4ba3e81e46112f7e484a42a978b
在那遥远的瞬间 <z351622948@163.com>
2019-10-20 16:15:06 +0800

    263. 丑数

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution2/Solution263.java
@@ -0,0 +1,55 @@
+package com.db117.example.leetcode.solution2;
+
+/**
+ * 263. 丑数
+ * 编写一个程序判断给定的数是否为丑数。
+ * <p>
+ * 丑数就是只包含质因数 2, 3, 5 的正整数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 6
+ * 输出: true
+ * 解释: 6 = 2 × 3
+ * 示例 2:
+ * <p>
+ * 输入: 8
+ * 输出: true
+ * 解释: 8 = 2 × 2 × 2
+ * 示例 3:
+ * <p>
+ * 输入: 14
+ * 输出: false
+ * 解释: 14 不是丑数，因为它包含了另外一个质因数 7。
+ * 说明：
+ * <p>
+ * 1 是丑数。
+ * 输入不会超过 32 位有符号整数的范围: [−231,  231 − 1]。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ugly-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/10/20/020 15:48
+ */
+public class Solution263 {
+    public boolean isUgly(int num) {
+        if (num < 1) {
+            return false;
+        }
+
+        // 能够除以 2 3 5 则递归调用
+        if (num % 2 == 0) {
+            return isUgly(num / 2);
+        }
+        if (num % 3 == 0) {
+            return isUgly(num / 3);
+        }
+        if (num % 5 == 0) {
+            return isUgly(num / 5);
+        }
+
+        return num == 1;
+    }
+}
