b9da8c98f33b82e580a2e1cec4558b38163c2572
在那遥远的瞬间 <z351622948@163.com>
2019-10-28 15:25:52 +0800

    405. 数字转换为十六进制数

--- leetcode/src/main/java/com/db117/example/leetcode/solution4/Solution405.java
+++ leetcode/src/main/java/com/db117/example/leetcode/solution4/Solution405.java
@@ -7,7 +7,8 @@ package com.db117.example.leetcode.solution4;
  * 注意:
  * <p>
  * 十六进制中所有字母(a-f)都必须是小写。
- * 十六进制字符串中不能包含多余的前导零。如果要转化的数为0，那么以单个字符'0'来表示；对于其他情况，十六进制字符串中的第一个字符将不会是0字符。 
+ * 十六进制字符串中不能包含多余的前导零。如果要转化的数为0，那么以单个字符'0'来表示；
+ * 对于其他情况，十六进制字符串中的第一个字符将不会是0字符。 
  * 给定的数确保在32位有符号整数范围内。
  * 不能使用任何由库提供的将数字直接转换或格式化为十六进制的方法。
  * 示例 1：
@@ -34,6 +35,22 @@ package com.db117.example.leetcode.solution4;
  */
 public class Solution405 {
     public String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        char[] chars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < 8 && num != 0) {
+            // 把后四位转换为16进制
+            sb.append(chars[num & 0xf]);
+            // 删除后四位
+            num >>= 4;
+        }
+        // 翻转
+        return sb.reverse().toString();
+    }
+
+    public String toHex1(int num) {
         return Integer.toHexString(num);
     }
 }
