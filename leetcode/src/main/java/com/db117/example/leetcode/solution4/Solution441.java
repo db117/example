43553edbfa516370bcc272298b50459b046c2ba1
在那遥远的瞬间 <z351622948@163.com>
2019-11-11 10:41:46 +0800

    441. 排列硬币

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution4/Solution441.java
@@ -0,0 +1,57 @@
+package com.db117.example.leetcode.solution4;
+
+/**
+ * 441. 排列硬币
+ * 你总共有 n 枚硬币，你需要将它们摆成一个阶梯形状，第 k 行就必须正好有 k 枚硬币。
+ * <p>
+ * 给定一个数字 n，找出可形成完整阶梯行的总行数。
+ * <p>
+ * n 是一个非负整数，并且在32位有符号整型的范围内。
+ * <p>
+ * 示例 1:
+ * <p>
+ * n = 5
+ * <p>
+ * 硬币可排列成以下几行:
+ * ¤
+ * ¤ ¤
+ * ¤ ¤
+ * <p>
+ * 因为第三行不完整，所以返回2.
+ * 示例 2:
+ * <p>
+ * n = 8
+ * <p>
+ * 硬币可排列成以下几行:
+ * ¤
+ * ¤ ¤
+ * ¤ ¤ ¤
+ * ¤ ¤
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/arranging-coins
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2019/11/11/011 10:35
+ */
+public class Solution441 {
+    public static void main(String[] args) {
+        System.out.println(new Solution441().arrangeCoins(5));
+    }
+
+    public int arrangeCoins(int n) {
+        if (n == 1) {
+            return n;
+        }
+        int count = 0;
+        int num = 1;
+        while (n >= num) {
+            n -= num++;
+            count++;
+        }
+        return count;
+    }
+
+}
