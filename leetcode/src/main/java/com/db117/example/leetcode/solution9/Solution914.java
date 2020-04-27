9e6b17d3fe5e6228414638633db8b203bd2dfb8c
在那遥远的瞬间 <z351622948@163.com>
2020-04-27 18:40:32 +0800

    914. 卡牌分组

---
+++ leetcode/src/main/java/com/db117/example/leetcode/solution9/Solution914.java
@@ -0,0 +1,81 @@
+package com.db117.example.leetcode.solution9;
+
+import java.util.Arrays;
+
+/**
+ * 914. 卡牌分组
+ * 给定一副牌，每张牌上都写着一个整数。
+ * <p>
+ * 此时，你需要选定一个数字 X，使我们可以将整副牌按下述规则分成 1 组或更多组：
+ * <p>
+ * 每组都有 X 张牌。
+ * 组内所有的牌上都写着相同的整数。
+ * 仅当你可选的 X >= 2 时返回 true。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：[1,2,3,4,4,3,2,1]
+ * 输出：true
+ * 解释：可行的分组是 [1,1]，[2,2]，[3,3]，[4,4]
+ * 示例 2：
+ * <p>
+ * 输入：[1,1,1,2,2,2,3,3]
+ * 输出：false
+ * 解释：没有满足要求的分组。
+ * 示例 3：
+ * <p>
+ * 输入：[1]
+ * 输出：false
+ * 解释：没有满足要求的分组。
+ * 示例 4：
+ * <p>
+ * 输入：[1,1]
+ * 输出：true
+ * 解释：可行的分组是 [1,1]
+ * 示例 5：
+ * <p>
+ * 输入：[1,1,2,2,2,2]
+ * 输出：true
+ * 解释：可行的分组是 [1,1]，[2,2]，[2,2]
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= deck.length <= 10000
+ * 0 <= deck[i] < 10000
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/x-of-a-kind-in-a-deck-of-cards
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author db117
+ * @date 2020/4/26/026 17:22
+ */
+public class Solution914 {
+
+    public boolean hasGroupsSizeX(int[] deck) {
+        Arrays.sort(deck);
+        int[] flag = new int[10000];
+        // 记录次数
+        for (int i : deck) {
+            flag[i]++;
+        }
+        // 次数的最大公约数
+        return Arrays.stream(flag)
+                .filter(value -> value != 0)
+                .reduce(this::gcd)
+                .getAsInt() > 1;
+    }
+
+    /**
+     * 最大公约数
+     */
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+}
